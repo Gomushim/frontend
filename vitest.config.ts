840cd7e9e0d57445890b0b457e30eb20b7c39ac1
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-06 12:37:43 +0900

    🔧 Chore: jsdom -> happy-dom으로 변경
    chore: jsdom -> happy-dom으로 변경

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export default defineConfig({
   test: {
-    environment: "jsdom", // 브라우저 환경이 필요한 경우 (ex: DOM 관련 테스트)
+    environment: "happy-dom", // 브라우저 환경이 필요한 경우 (ex: DOM 관련 테스트)
     coverage: {
       reporter: ["text", "json", "html"], // 커버리지 리포트 형식
       exclude: ["node_modules/", "dist/", "tests/helpers/"], // 커버리지에서 제외할 파일
