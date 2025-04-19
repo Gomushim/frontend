f2c429bfc2ef05177f77c403d9c0810ea2e196e1
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-20 02:08:04 +0900

    ✨ Feat: route 추가설정
    feat: route 추가설정

--- src/utils/router/routes.tsx
+++ src/utils/router/routes.tsx
@@ -1,20 +1,36 @@
 import App from "@/App";
-import { Calender, Home } from "@/pages";
+import {
+  CalendarDdayList,
+  CalendarNewSchedule,
+  CalendarRoot,
+  CalendarScheduleDetail,
+  Home,
+  Login,
+  MyPage,
+  Onboarding,
+  ProtectedLayout,
+} from "@/pages";
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
+  { path: "/login", element: <Login /> }, // 로그인 페이지
+  { path: "/onboarding", element: <Onboarding /> }, // 온보딩(회원가입) 페이지
   {
     path: "/",
-    element: <App />,
+    element: <ProtectedLayout />, //  로그인한 사용자만 접근 가능한 보호 레이아웃
     children: [
       {
-        index: true,
-        element: <Home />,
+        path: "",
+        element: <App />, // 공통 레이아웃
+        children: [
+          { index: true, element: <Home /> }, // 메인 홈 화면
+          { path: "calendar", element: <CalendarRoot /> }, // 캘린더 메인 화면
+          { path: "mypage", element: <MyPage /> }, // 마이페이지
+        ],
       },
-      {
-        path: "calender",
-        element: <Calender />,
-      },
+      { path: "calendar/schedule", element: <CalendarNewSchedule /> }, // 일정 생성 페이지
+      { path: "calendar/:scheduleId", element: <CalendarScheduleDetail /> }, // 특정 일정 상세 페이지 (동적 라우팅)
+      { path: "calendar/dday", element: <CalendarDdayList /> }, //D-Day 일정 리스트 페이지
     ],
   },
 ]);
