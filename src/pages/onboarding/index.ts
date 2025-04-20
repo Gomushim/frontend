01da25c0cb17c5dba97a5986dd88383220bf5f03
yumin <umin030312@naver.com>
2025-04-20 19:55:42 +0900

    🚚 routes : 온보딩 라우트

---
+++ src/pages/onboarding/index.ts
@@ -0,0 +1,6 @@
+export * from "./Onboarding";
+export * from "./CoupleContact/FirstMeet"
+export * from "./CoupleContact/MilitaryDay"
+export * from "./CoupleContact/CoupleContact"
+export * from "./components/DatePickerModal"
+export * from "./components/ProgressHeader"
--- src/utils/router/routes.tsx
+++ src/utils/router/routes.tsx
@@ -11,17 +11,19 @@ import {
   ProtectedLayout,
 } from "@/pages";
 import { createBrowserRouter } from "react-router";
+import { FirstMeet } from "@/pages/onboarding";
+import { MilitaryDay } from "@/pages/onboarding";
+import { CoupleContact } from "@/pages/onboarding/CoupleContact/CoupleContact";
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> }, // 로그인 페이지
   { path: "/onboarding", element: <Onboarding /> }, // 온보딩(회원가입) 페이지
   {
     path: "/",
-    element: <ProtectedLayout />, //  로그인한 사용자만 접근 가능한 보호 레이아웃
     children: [
       {
         path: "",
-        element: <App />, // 공통 레이아웃
+        element: <FirstMeet />, // 공통 레이아웃
         children: [
           { index: true, element: <Home /> }, // 메인 홈 화면
           { path: "calendar", element: <CalendarRoot /> }, // 캘린더 메인 화면
@@ -30,8 +32,11 @@ const router = createBrowserRouter([
       },
       { path: "calendar/schedule", element: <CalendarNewSchedule /> }, // 일정 생성 페이지
       { path: "calendar/:scheduleId", element: <CalendarScheduleDetail /> }, // 특정 일정 상세 페이지 (동적 라우팅)
-      { path: "calendar/dday", element: <CalendarDdayList /> }, //D-Day 일정 리스트 페이지
+      { path: "calendar/dday", element: <CalendarDdayList /> }, //D-Day 일정 리스트 페이지 
+      { path: "/onboarding/military-day", element: <MilitaryDay /> }, //D-Day 일정 리스트 페이지
+      { path: "/onboarding/couple-contact", element: <CoupleContact /> }, //D-Day 일정 리스트 페이지
     ],
+   
   },
 ]);
 export default router;
