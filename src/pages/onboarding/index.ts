5ec87b03ba0bc79cfdbfbef093cbfefd9d3b0096
yumin <umin030312@naver.com>
2025-04-20 20:17:27 +0900

    🚚 routes : 온보딩 라우트 수정

--- src/pages/onboarding/index.ts
+++ src/pages/onboarding/index.ts
@@ -1,6 +1,5 @@
-export * from "./Onboarding";
+export * from "./Onboarding"
 export * from "./CoupleContact/FirstMeet"
 export * from "./CoupleContact/MilitaryDay"
 export * from "./CoupleContact/CoupleContact"
-export * from "./components/DatePickerModal"
 export * from "./components/ProgressHeader"
