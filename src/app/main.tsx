6095f5562b270b0818788b9c461ea0f892e30984
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-25 00:39:42 +0900

    🔧 Chore: react query devtools 적용
    chore: react query devtools 적용

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router";
 import { StrictMode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router/routes";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
