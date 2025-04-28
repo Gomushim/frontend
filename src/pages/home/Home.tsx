844d3fd2db1467ac12639bd7d03573511be29cf0
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-28 23:12:10 +0900

    🐛 Fix: 코드 수정
    fix: 코드 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,17 @@
 import { requestNotificationPermission } from "@/shared/firebase/setupFCM";
+import { useState } from "react";
 
 export const Home = () => {
+  const [text, setText] = useState<string | null>("");
+
   const onClick = async () => {
     const test = await requestNotificationPermission();
+    setText(test);
     console.log(test);
   };
-  return <div onClick={onClick}>Home</div>;
+  return (
+    <div onClick={onClick}>
+      <p>{text}</p>
+    </div>
+  );
 };
