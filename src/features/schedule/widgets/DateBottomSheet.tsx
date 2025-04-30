0a15aaeb8903302e4fd63f15f35cd787b6c3cb59
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-30 18:48:28 +0900

    ♻️ Refactor: UI 컴포넌트 대소문자 통일
    refactor: UI 컴포넌트 대소문자 통일

--- src/features/schedule/widgets/DateBottomSheet.tsx
+++ src/features/schedule/widgets/DateBottomSheet.tsx
@@ -2,6 +2,7 @@ import { DatePickerDrawer, DateSelector } from "@/shared/ui";
 
 const handleDateConfirm = (date: Date) => {
   // setBirthday(formatDateKorean(date));
+  console.log("date", date);
 };
 
 export const DateBottomSheet = () => {
