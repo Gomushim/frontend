3c0f2de490f61d5d156b2e780335c6ee3ad0d520
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-30 23:03:57 +0900

    🐛 Fix: 스타일 오류 해결
    fix: 스타일 오류 해결

--- src/pages/calendar/NewSchedule.tsx
+++ src/pages/calendar/NewSchedule.tsx
@@ -29,9 +29,7 @@ export const CalendarNewSchedule = () => {
   return (
     <>
       <header className="mt-[70px] mb-8 flex flex-col items-center gap-7">
-        <h1 text-gary-900 text-xl font-semibold>
-          생성하기
-        </h1>
+        <h1 className="text-xl font-semibold text-gray-900">생성하기</h1>
         <Topbar isToggle={isToggle} onToggle={onToggle} />
       </header>
       <main className="flex flex-col gap-6 p-5">
@@ -43,11 +41,11 @@ export const CalendarNewSchedule = () => {
           <>
             <section className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
-                <h3 className="text-gary-900 text-xl font-semibold">이모티콘</h3>
+                <h3 className="text-xl font-semibold text-gray-900">이모티콘</h3>
                 <EmojiSelector />
               </div>
               <div className="flex flex-col gap-2">
-                <h3 className="text-gary-900 text-xl font-semibold">날짜</h3>
+                <h3 className="text-xl font-semibold text-gray-900">날짜</h3>
                 <DateBottomSheet type="dday" />
               </div>
             </section>
@@ -61,23 +59,23 @@ export const CalendarNewSchedule = () => {
           <>
             <section className="flex flex-col gap-4">
               <div className="flex items-center justify-between">
-                <h3 className="text-gary-900 text-xl font-semibold">하루종일</h3>
+                <h3 className="text-xl font-semibold text-gray-900">하루종일</h3>
                 <AllDayToggleButton />
               </div>
               <div className="grid grid-cols-[1fr_112px_112px] gap-2">
-                <h3 className="text-gary-900 text-xl font-semibold">시작</h3>
+                <h3 className="text-xl font-semibold text-gray-900">시작</h3>
                 <DateBottomSheet type="start" />
                 <TimeBottomSheet target="start" />
               </div>
               <div className="grid grid-cols-[1fr_112px_112px] gap-2">
-                <h3 className="text-gary-900 text-xl font-semibold">종료</h3>
+                <h3 className="text-xl font-semibold text-gray-900">종료</h3>
                 <DateBottomSheet type="end" />
                 <TimeBottomSheet target="end" />
               </div>
             </section>
             <Divider thickness="h-px" color="bg-gray-100" />
             <section className="flex items-center justify-between">
-              <h3 className="text-gary-900 text-xl font-semibold">피로도 선택</h3>
+              <h3 className="text-xl font-semibold text-gray-900">피로도 선택</h3>
               <FatigueBottomSheet />
             </section>
             <section className="fixed bottom-6 left-1/2 w-[375px] -translate-x-1/2 transform px-4">
