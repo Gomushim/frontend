f31a69ca4c9d065050f44c1dfb7359cb9f934b15
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-03 00:41:43 +0900

    ✨ Feat: d-day 카드 나올 수 있게 로직 추가
    feat: d-day 카드 나올 수 있게 로직 추가

--- src/features/schedule/widgets/ScheduleCardList.tsx
+++ src/features/schedule/widgets/ScheduleCardList.tsx
@@ -2,6 +2,7 @@ import { useSelectedDateStore } from "@/entities/schedule";
 import { useGetScheduleList } from "@/entities/schedule/query";
 import { ScheduleCard } from "./ScheduleCard";
 import { formatDateKoreanWithWeekday } from "@/shared/utils";
+import { DdayCard } from "@/features/d-day/widgets";
 
 export const ScheduleCardList = () => {
   const { selectedDay } = useSelectedDateStore();
@@ -9,6 +10,7 @@ export const ScheduleCardList = () => {
   return (
     <>
       <h2>{formatDateKoreanWithWeekday(selectedDay)}</h2>
+      {scheduleListData?.result.anniversaries.map(dday => <DdayCard key={dday.id} {...dday} />)}
       {scheduleListData?.result.schedules.map(schedule => <ScheduleCard key={schedule.id} {...schedule} />)}
     </>
   );
