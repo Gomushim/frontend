4289d60b4390f5ab04cd46b711b4a0315020a14c
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-04 17:48:59 +0900

    ✨ Feat: 디데이 리스트 불러오는 API 로직 작성 및 연결
    feat: 디데이 리스트 불러오는 API 로직 작성 및 연결

---
+++ src/entities/d-day/query.ts
@@ -0,0 +1,21 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { ddayQueryKey } from "./queryKey";
+import { getDdayList } from "./service";
+
+export const useGetDdayList = () => {
+  return useInfiniteQuery({
+    ...ddayQueryKey.list(),
+    queryFn: async ({ pageParam = 1 }) => {
+      return getDdayList({
+        key: pageParam,
+        orderCreatedAt: "desc",
+        take: 10,
+      });
+    },
+    initialPageParam: 1,
+    getNextPageParam: lastPage => {
+      const { isLastPage, after } = lastPage;
+      return isLastPage ? undefined : after;
+    },
+  });
+};
--- src/entities/d-day/service.ts
+++ src/entities/d-day/service.ts
@@ -1,7 +1,21 @@
 import { api } from "../axios/instance";
-import { DdayRequst, NewDdayResponse } from "./type";
+import { DDayListResponse, DdayRequst, NewDdayResponse } from "./type";
 
 export const createDday = async (data: DdayRequst): Promise<NewDdayResponse> => {
   const response = await api.post<NewDdayResponse>("/couple/new-anniversary", data);
+  return response.data;
+};
+
+export const getDdayList = async ({
+  orderCreatedAt,
+  take,
+}: {
+  key: number;
+  orderCreatedAt: "asc" | "desc";
+  take: number;
+}): Promise<DDayListResponse> => {
+  const response = await api.get<DDayListResponse>("/anniversaries", {
+    params: { orderCreatedAt: orderCreatedAt, take: take },
+  });
   return response.data;
 };
