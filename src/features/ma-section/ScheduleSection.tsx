ab127ae7a35aa36abaf2eaa1bf59450c2ef27b31
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-05 13:57:50 +0900

    ♻️ Refactor: 메인 캘린더에서 쓸 컴포넌트 생성 및 디자인 수정
    refactor: 메인 캘린더에서 쓸 컴포넌트 생성 및 디자인 수정

--- src/features/ma-section/ScheduleSection.tsx
+++ src/features/ma-section/ScheduleSection.tsx
@@ -1,19 +1,17 @@
 import { Suspense } from "react";
-import { MiniCalendar } from "../schedule";
+import { MainScheduleCardList, MiniCalendar } from "../schedule";
 import { MainHeader } from "./utils/MainHeader";
-
-import { ScheduleCardList } from "../schedule/widgets/ScheduleCardList";
 
 export const ScheduleSection = () => {
   return (
     <>
       <MainHeader mainTitle="우리의 일정" onClick={() => console.log("일정 더보기")} />
-      <section className="mx-auto max-w-md rounded-2xl bg-white p-4">
+      <section className="mx-auto w-full max-w-md rounded-2xl bg-white py-4">
         <Suspense fallback={<div>로딩중</div>}>
           <MiniCalendar />
         </Suspense>
         <Suspense fallback={<div>로딩중...</div>}>
-          <ScheduleCardList />
+          <MainScheduleCardList />
         </Suspense>
       </section>
     </>
---
+++ src/features/schedule/widgets/MainScheduleCardList.tsx
@@ -0,0 +1,19 @@
+import { useSelectedDateStore } from "@/entities/schedule";
+import { useGetScheduleList } from "@/entities/schedule/query";
+import { formatDateKoreanWithWeekday } from "@/shared/utils";
+import { ScheduleCard } from "./ScheduleCard";
+import { DdayCard } from "@/features/d-day/widgets";
+
+export const MainScheduleCardList = () => {
+  const { selectedDay } = useSelectedDateStore();
+  const { data: scheduleListData } = useGetScheduleList(selectedDay);
+  return (
+    <div className="flex flex-col gap-3 px-4">
+      <h2>{formatDateKoreanWithWeekday(selectedDay)}</h2>
+      {scheduleListData?.result.anniversaries.map(dday => <DdayCard key={dday.id} {...dday} className="bg-gray-50" />)}
+      {scheduleListData?.result.schedules.map(schedule => (
+        <ScheduleCard key={schedule.id} {...schedule} className="bg-gray-50" />
+      ))}
+    </div>
+  );
+};
