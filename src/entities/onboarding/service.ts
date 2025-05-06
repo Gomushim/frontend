d64658512eb9f338ba085c11317a2c0491559c6f
yumin <umin030312@naver.com>
2025-05-06 21:59:02 +0900

    🐛 fix : api 오류 수정

--- src/entities/onboarding/service.ts
+++ src/entities/onboarding/service.ts
@@ -1,14 +1,25 @@
 import { OnboardingRequest, OnboardingResponse } from "./types";
 import { api } from "../axios/instance";
-import { formatDateKorean } from "@/shared/utils/date/formatdate";
+
+function parseKoreanDateString(dateStr: string): string {
+  return dateStr
+    .replace("년", "-")
+    .replace("월", "-")
+    .replace("일", "")
+    .replace(/\s/g, "")
+    .replace(/-+/g, "-");
+}
 
 export const onboardingQueries = {
   postOnboarding: async (data: OnboardingRequest): Promise<OnboardingResponse> => {
+    let birthDate = data.birthDate;
+    if (birthDate.includes("년")) {
+      birthDate = parseKoreanDateString(birthDate);
+    }
     const formattedData = {
       ...data,
-      birthDate: formatDateKorean(new Date(data.birthDate)),
+      birthDate: new Date(birthDate).toISOString().slice(0, 10),
     };
-    
     const response = await api.post<OnboardingResponse>("/member/onboarding", formattedData);
     return response.data;
   },
