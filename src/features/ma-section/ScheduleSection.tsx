3ffd153b266c48b4f6d63241028a5c2c3b82e766
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-06 21:52:32 +0900

    ✨ Feat: 에러 바운더리 설정
    feat: 에러 바운더리 설정

--- src/features/ma-section/ScheduleSection.tsx
+++ src/features/ma-section/ScheduleSection.tsx
@@ -1,18 +1,24 @@
 import { Suspense } from "react";
 import { MainScheduleCardList, MiniCalendar } from "../schedule";
 import { MainHeader } from "./utils/MainHeader";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const ScheduleSection = () => {
   return (
     <>
       <MainHeader mainTitle="우리의 일정" onClick={() => console.log("일정 더보기")} />
       <section className="mx-auto w-full max-w-md rounded-2xl bg-white py-4">
-        <Suspense fallback={<div>로딩중</div>}>
-          <MiniCalendar />
-        </Suspense>
-        <Suspense fallback={<div>로딩중...</div>}>
-          <MainScheduleCardList />
-        </Suspense>
+        <ErrorBoundary fallback={<div>에러 발생</div>}>
+          <Suspense fallback={<div>로딩중</div>}>
+            <MiniCalendar />
+          </Suspense>
+        </ErrorBoundary>
+
+        <ErrorBoundary fallback={<div>에러 발생</div>}>
+          <Suspense fallback={<div>로딩중...</div>}>
+            <MainScheduleCardList />
+          </Suspense>
+        </ErrorBoundary>
       </section>
     </>
   );
