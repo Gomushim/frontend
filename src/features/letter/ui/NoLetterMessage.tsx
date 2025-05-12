aedfac8439171d89d105879094db4b9e571d82f5
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-13 01:45:21 +0900

    💄 Design: NoLetterMessage 컴포넌트 화면 꽉차게 스타일 수정
    design: NoLetterMessage 컴포넌트 화면 꽉차게 스타일 수정

--- src/features/letter/ui/NoLetterMessage.tsx
+++ src/features/letter/ui/NoLetterMessage.tsx
@@ -2,8 +2,8 @@ import mailbox from "@/assets/images/mailbox.svg";
 
 export const NoLetterMessage = () => {
   return (
-    <div className="flex min-h-[500px] flex-col items-center">
-      <img className="mt-[70px]" src={mailbox} alt="편지 없음" />
+    <div className="flex h-full min-h-[calc(100vh-400px)] flex-col items-center justify-center">
+      <img src={mailbox} alt="편지 없음" />
       <h3 className="mt-4 text-2xl font-semibold text-gray-800">아직 편지가 없네요!</h3>
       <p className="text-md mt-2 text-center font-medium text-gray-400">
         기다리는 연인을 위해
