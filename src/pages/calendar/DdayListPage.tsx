313ea977dd7a51143bd48e98bcd484631a587158
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-13 16:00:49 +0900

    ✨ Feat: 유저명 연결 및 디데이 총 갯수 연결
    feat: 유저명 연결 및 디데이 총 갯수 연결

--- src/pages/calendar/DdayListPage.tsx
+++ src/pages/calendar/DdayListPage.tsx
@@ -6,9 +6,13 @@ import calendar from "@/assets/icons/calendar.svg";
 import blackHeart from "@/assets/icons/blackHeart.svg";
 import { useNavigate } from "react-router";
 import { DdayCard, NoDdayMessage } from "@/features/d-day/ui";
+import { useCoupleNickname } from "@/entities/couple_nickname";
 
 export const CalendarDdayList = () => {
   const { data: ddayListData, fetchNextPage, isFetched } = useGetDdayList();
+  const { getNickName } = useCoupleNickname();
+  const { data: coupleNickname } = getNickName;
+
   const ref = useIntersect<HTMLDivElement>(() => {
     fetchNextPage();
   }, isFetched);
@@ -40,11 +44,13 @@ export const CalendarDdayList = () => {
         <div className="mt-4.5 flex items-center justify-between">
           <div className="items-cente flex gap-2">
             <img className="h-4.5 w-4.5 pt-1" src={calendar} alt="캘린더 아이콘" />
-            <h2 className="text-xl font-semibold text-gray-900">산들</h2>
+            <h2 className="text-xl font-semibold text-gray-900">{coupleNickname?.result.userNickname}</h2>
             <img src={blackHeart} alt="캘린더 아이콘" />
-            <h2 className="text-xl font-semibold text-gray-900">세린</h2>
+            <h2 className="text-xl font-semibold text-gray-900">{coupleNickname?.result.coupleNickname}</h2>
             <h2 className="text-xl font-semibold text-gray-900">님의 디데이</h2>
-            <p className="text-md font-semibold text-gray-500">2</p>
+            <p className="text-md font-semibold text-gray-500">
+              {ddayListData.pages.reduce((total, page) => total + page.data.length, 0)}
+            </p>
           </div>
           <Button variant="square" size="2xs" onClick={goCreateDdayPage}>
             디데이 추가
