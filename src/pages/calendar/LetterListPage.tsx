05e5df1b83fe34b00b62333dc59d88f091791b09
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-13 15:23:47 +0900

    🐛 Fix: 편지 리스트 없을 때 나오는 UI 조건 수정
    fix: 편지 리스트 없을 때 나오는 UI 조건 수정

--- src/pages/calendar/LetterListPage.tsx
+++ src/pages/calendar/LetterListPage.tsx
@@ -5,9 +5,13 @@ import blackHeart from "@/assets/icons/blackHeart.svg";
 import { MonthlyLettersView, NoLetterMessage } from "@/features/letter";
 import { useGetLetterList } from "@/entities/letter/query";
 import useIntersect from "@/shared/hooks/useIntersect";
+import { useCoupleNickname } from "@/entities/couple_nickname";
 
 export const LetterListPage = () => {
   const { data: letterListData, fetchNextPage, isFetched } = useGetLetterList();
+  const { getNickName } = useCoupleNickname();
+  const { data: coupleNickname } = getNickName;
+
   const ref = useIntersect<HTMLDivElement>(() => {
     fetchNextPage();
   }, isFetched);
@@ -25,18 +29,20 @@ export const LetterListPage = () => {
         <h1 className="pt-[70px]">편지</h1>
       </header>
       <main className="mt-4.5">
-        <section className="mb-6 flex items-center gap-2">
-          <img className="h-5 w-5 pb-1" src={letterIcon} alt="캘린더 아이콘" />
-          <h2 className="text-xl font-semibold text-gray-900">세린</h2>
-          <img src={blackHeart} alt="캘린더 아이콘" />
-          <h2 className="text-xl font-semibold text-gray-900">산들</h2>
-          <h2 className="text-xl font-semibold text-gray-900">님의 편지</h2>
-          <p className="text-md font-semibold text-gray-500">2</p>
-        </section>
+        {letterListData.pages[0].data.length > 0 && (
+          <section className="mb-6 flex items-center gap-2">
+            <img className="h-5 w-5 pb-1" src={letterIcon} alt="캘린더 아이콘" />
+            <h2 className="text-xl font-semibold text-gray-900">{coupleNickname?.result.userNickname}</h2>
+            <img src={blackHeart} alt="캘린더 아이콘" />
+            <h2 className="text-xl font-semibold text-gray-900">{coupleNickname?.result.coupleNickname}</h2>
+            <h2 className="text-xl font-semibold text-gray-900">님의 편지</h2>
+            <p className="text-md font-semibold text-gray-500">2</p>
+          </section>
+        )}
         {letterListData.pages.map(page => (
           <MonthlyLettersView key={page.after} {...page} />
         ))}
-        {letterListData.pages.length === 0 && <NoLetterMessage />}
+        {letterListData.pages[0].data.length === 0 && <NoLetterMessage />}
         <div ref={ref} style={{ width: "1px", height: "1px", marginTop: "10px" }} />
       </main>
     </div>
