429b6232935f2f1063ebc75b3ddd684557baf550
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-17 22:23:11 +0900

    🐛 Fix: enabled 매개변수 옵셔널 처리
    fix: enabled 매개변수 옵셔널 처리

--- src/entities/couple_nickname/queries.ts
+++ src/entities/couple_nickname/queries.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { coupleNicknameQueries } from "./service";
 
-export const useCoupleNickname = (enabled: boolean) => {
+export const useCoupleNickname = (enabled?: boolean) => {
   const getNickName = useQuery({
     queryKey: ["coupleNickname"],
     queryFn: () => coupleNicknameQueries.getNickName(),
