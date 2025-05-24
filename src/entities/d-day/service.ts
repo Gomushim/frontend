9b33b52500e383b93abe2a5c61be025eafa3d987
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-24 15:15:39 +0900

    ✨ Feat: 디데이 삭제 API 연결
    feat: 디데이 삭제 API 연결

--- src/entities/d-day/service.ts
+++ src/entities/d-day/service.ts
@@ -1,5 +1,5 @@
 import { api } from "../axios/instance";
-import { DDayListResponse, DdayRequst, MainDdayListResponse, NewDdayResponse } from "./model";
+import { DDayListResponse, DdayRequst, DeleteDdayResponse, MainDdayListResponse, NewDdayResponse } from "./model";
 
 export const createDday = async (data: DdayRequst): Promise<NewDdayResponse> => {
   const response = await api.post<NewDdayResponse>("/couple/new-anniversary", data);
@@ -19,7 +19,11 @@ export const getMainDdayList = async (): Promise<MainDdayListResponse> => {
 };
 
 export const updateDday = async (data: DdayRequst): Promise<NewDdayResponse> => {
-  console.log("data", data);
-  const response = await api.put<NewDdayResponse>("/couple/new-anniversary", data);
+  const response = await api.post<NewDdayResponse>("/couple/new-anniversary", data);
+  return response.data;
+};
+
+export const deleteDday = async (ddayId: string): Promise<DeleteDdayResponse> => {
+  const response = await api.delete<DeleteDdayResponse>(`/anniversary/${ddayId}`);
   return response.data;
 };
