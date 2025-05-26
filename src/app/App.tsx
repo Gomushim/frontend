628c3a868681371b425e4cdc4aae2a96d9004c99
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-26 21:59:31 +0900

    🐛 Fix: 스크롤 다시 수정
    fix: 스크롤 다시 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router";
 function App() {
   return (
-    <div className="pc:flex min-h-screen justify-center overflow-hidden bg-gray-500">
-      <div className="scrollbar-hide pc:max-w-[375px] relative h-screen w-full overflow-y-auto bg-white">
+    <div className="pc:flex min-h-screen justify-center bg-gray-500">
+      <div className="scrollbar-hide pc:max-w-[375px] relative w-full bg-white">
         <Outlet />
       </div>
     </div>
