3081656f8e6e7826203fae53fb8efa77f8bb991e
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-29 23:20:52 +0900

    💄 Style: 탑 마진 설정
    style: 탑 마진 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 function App() {
   return (
     <div className="pc:flex h-screen justify-center overflow-hidden bg-gray-500">
-      <div className="pc:max-w-[375px] relative h-screen w-full overflow-y-auto bg-white">
+      <div className="pc:max-w-[375px] bg-whitept-[env(safe-area-inset-top)] relative h-screen w-full overflow-y-auto">
         <Outlet />
       </div>
     </div>
