92df425d1947386326c3256b169ee7abff83bcbf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-06-05 12:49:30 +0900

    🔥 Remove: 서비스워커 등록 코드 제거
    remove: 서비스워커 등록 코드 제거1

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -25,28 +25,29 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </StrictMode>
 );
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser");
-  worker.start({
-    onUnhandledRequest: "bypass",
-    serviceWorker: {
-      url: "/mockServiceWorker.js",
-      options: {
-        scope: "/api/",
-      },
-    },
-  });
-}
-// service worker 등록
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(reg => {
-        console.log("Service worker registered:", reg);
-      })
-      .catch(err => {
-        console.error("Service worker registration failed:", err);
-      });
-  });
-}
+
+// if (import.meta.env.DEV) {
+//   const { worker } = await import("./mocks/browser");
+//   worker.start({
+//     onUnhandledRequest: "bypass",
+//     serviceWorker: {
+//       url: "/mockServiceWorker.js",
+//       options: {
+//         scope: "/api/",
+//       },
+//     },
+//   });
+// }
+// // service worker 등록
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/sw.js")
+//       .then(reg => {
+//         console.log("Service worker registered:", reg);
+//       })
+//       .catch(err => {
+//         console.error("Service worker registration failed:", err);
+//       });
+//   });
+// }
